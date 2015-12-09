6f292c64fa8b9efbcebed8232096504b8d5df353
omidhabedy <omidiskawous@gmail.com>
2015-12-09 13:14:07 -0600

    updated

---
+++ includes/social_media.php
@@ -0,0 +1,18 @@
+ <div class="social-media">
+    <div class="social-media-inner container text-center">
+        <ul class="list-inline">
+            <li class="twitter-follow"><a href="https://twitter.com/aikyerim" class="twitter-follow-button" data-show-count="false">Follow @aikyerim</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            </li><!--//twitter-follow-->
+            <li class="twitter-tweet">
+                <a href="https://twitter.com/share" class="twitter-share-button" data-via="3rdwave_themes" data-hashtags="bootstrap">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            </li><!--//twitter-tweet-->
+          
+            <!--//
+            <li class="github-follow"><iframe src="http://ghbtns.com/github-btn.html?user=mdo&type=follow&count=true"
+allowtransparency="true" frameborder="0" scrolling="0" width="165" height="20"></iframe></li>
+            -->
+        </ul>
+    </div>
+</div>
